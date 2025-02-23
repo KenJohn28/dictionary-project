1b08df379b60edf6ee19f1db8aa5ecffdc524626
KenJohn28 <johnsonmok1129@gmail.com>
2025-02-23 12:05:45 -0500

    Added Dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary.js";
@@ -7,17 +8,15 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-        <img src={logo} className="App-logo img-fluid"
-        alt="logo" />
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-     <footer className="App-footer">
-      <small>Coded by Kenya Johnson</small> 
-     </footer>
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Kenya Johnson</small>
+        </footer>
       </div>
     </div>
   );
 }
-
