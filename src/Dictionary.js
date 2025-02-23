8044165a8500b467ea632c3812cd69cdfaee82c3
KenJohn28 <johnsonmok1129@gmail.com>
2025-02-23 12:06:42 -0500

    Added API documentation URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,9 +12,10 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    //https://api.dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    
-    axios.get(apiUrl).then(handleResponse)
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
 
