a5a53020668f5ecd10e624e45c5acd92e7eaac90
KenJohn28 <johnsonmok1129@gmail.com>
2025-02-23 13:40:48 -0500

    Automatically formatted after save

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,10 +6,11 @@ export default function Meaning(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map((definition, index) => (
         <div key={index}>
-          <p>{definition.definition}
-          
-          <br />
-          {definition.example && <em>{definition.example}</em>}
+          <p>
+            {definition.definition}
+
+            <br />
+            {definition.example && <em>{definition.example}</em>}
           </p>
         </div>
       ))}
