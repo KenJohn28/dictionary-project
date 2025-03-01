3057341cef318f84c25ecabd48e00d96491fdd7e
KenJohn28 <johnsonmok1129@gmail.com>
2025-03-01 17:00:57 -0500

    Created Synonym component
    stores all synonym information that will be pulled in by the Meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,22 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map((definition, index) => (
         <div key={index}>
           <p>
-            {definition.definition}
-
+            <strong>Definition:</strong> {definition.definition}
             <br />
+            <strong>Example:</strong>{" "}
             {definition.example && <em>{definition.example}</em>}
           </p>
+          {/* Move Synonyms outside of <p> */}
+          <strong>Synonyms:</strong>
+          <Synonyms synonyms={props.meaning.synonyms} />
         </div>
       ))}
     </div>
