dc27d13d74cb47ce984d2df01440f58d2cfe1155
KenJohn28 <johnsonmok1129@gmail.com>
2025-03-23 19:41:50 -0400

    Corrected return statement

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
-  return (<div className="Phonetic">
-    <a href="{props.phonetic.audio}" target="_blank">
-    listen
-    </a>
-    <br/>
-    {props.phonetic.text}
-  </div>);
+  return (
+    <div className="Phonetic">
+      <a href="{props.phonetic.audio}" target="_blank">
+        listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
 }
